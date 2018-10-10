86a2555d1ff7c47bb4c1ee87c94551d6b8f46c39
lovekun <986970575@qq.com>
2018-10-10 23:29:06 +0800

    add move, delete, copy,

--- qiniuCommandLine.py
+++ qiniuCommandLine.py
@@ -10,43 +10,41 @@ import argparse
 import json
 import requests
 
-domain = "pf388se7v.bkt.clouddn.com"
 access_key = 'vr70Yo2pV5Ffp0YbEgnMjSa_EPgvthnci_VxiRs0'
 secret_key = 'Dx2X_tZg-wCekIbRjwRijf3_C9fmwC7heCkfae5v'
 
-def upload(localfile, key, bucket='document'):
+def upload(localfile, key, bucket):
     q = Auth(access_key, secret_key)
     bucket_name = bucket
     key = key
     token = q.upload_token(bucket_name, key, 3600)
     localfile = localfile
+    result = http._get("http://api.qiniu.com/v6/domain/list?tbl=" + bucket, None, q)
+    base_url = "http://" + result[0][0] + "/" + key
     ret, info = put_file(token, key, localfile)
     if info.status_code == 200:
-        link = "http://" + domain + key
+        result = http._get("http://api.qiniu.com/v6/domain/list?tbl=" + bucket, None, q)
+        link = "http://" + result[0][0] + "/" + key
         return link
     else:
         return "upload error"
 
 def query(bucket_name, key):
-    #初始化Auth状态
     q = Auth(access_key, secret_key)
-    #初始化BucketManager
     bucket = BucketManager(q)
-    #你要测试的空间， 并且这个key在你空间中存在
-    # bucket_name = 'document'
-    # key = 'mytest.jpg'
-    #获取文件的状态信息
     ret, info = bucket.stat(bucket_name, key)
-    print(info)
+    print ret
+    # print(json.dumps(ret, sort_keys=True, indent=4))
+    # print(info)
 
-def queryList(bucket_name):
+def queryList(bucket_name, prefix_name):
     q = Auth(access_key, secret_key)
     bucket = BucketManager(q)
-    # bucket_name = 'lovekun'
     # 前缀
-    prefix = None
+    prefix = prefix_name
     # 列举条目
     # limit = 10
+    limit = None
     # 列举出除'/'的所有文件以及以'/'为分隔的所有前缀
     delimiter = None
     # 标记
@@ -64,7 +62,6 @@ def queryBucketDomain(bucket_name):
     result = http._get("http://api.qiniu.com/v6/domain/list?tbl=" + bucket_name, None, q)
     print result[0][0]
 
-
 def download(bucket, key):
     q = Auth(access_key, secret_key)
     result = http._get("http://api.qiniu.com/v6/domain/list?tbl=" + bucket, None, q)
@@ -75,16 +72,36 @@ def download(bucket, key):
     r = requests.get(private_url)
     assert r.status_code == 200
 
-def delete(bucket, key):
-    pass
+def delete(bucket_name, key):
+    q = Auth(access_key, secret_key)
+    bucket = BucketManager(q)
+    ret, info = bucket.delete(bucket_name, key)
+    print(info)
+    assert ret == {}
+
+# if move between the same bucket, then method can used to rename the file key
+def move(src_bucket, src_key, des_bucket, des_key):
+    q = Auth(access_key, secret_key)
+    bucket = BucketManager(q)
+    ret, info = bucket.move(src_bucket, src_key, des_bucket, des_key)
+    print(info)
+    assert ret == {}
+
+def copy(src_bucket, src_key, des_bucket, des_key):
+    q = Auth(access_key, secret_key)
+    bucket = BucketManager(q)
+    ret, info = bucket.copy(src_bucket, src_key, des_bucket, des_key)
+    print(info)
+    assert ret == {}
 
 def cmd():
     args = argparse.ArgumentParser(description = 'QiNiuYun operation tools',epilog = 'Information end ')
-    args.add_argument("-o", "--operation", type = str, dest = "operation", help = "specify operation", default = "upload", choices=["upload", "query", "queryList", "queryBuckets", "queryBucketDomain", "download"])
+    args.add_argument("-o", "--operation", type = str, dest = "operation", help = "specify operation", default = "upload", choices=["upload", "query", "queryList", "queryBuckets", "queryBucketDomain", "download", "delete", "move"])
     args.add_argument("-f", "--file", type = str, help = "the file to upload")
     args.add_argument("-k", "--key", type = str, help = "the key for the file to upload")
     args.add_argument("-b", "--bucket", type = str, help = "upload the file to specify bucket")
-    
+    args.add_argument("-c", "--condition", type = str, help = "query list by condition")
+
     args = args.parse_args()
     d = args.__dict__
     if d["operation"] == "upload":
@@ -92,13 +109,15 @@ def cmd():
     elif d["operation"] == "query":
         query(d["bucket"], d["key"])
     elif d["operation"] == "queryList":
-        queryList(d["bucket"])
+        queryList(d["bucket"], d["condition"])
     elif d["operation"] == "queryBuckets":
         queryBuckets()
     elif d["operation"] == "queryBucketDomain":
         queryBucketDomain(d["bucket"])
     elif d["operation"] == "download":
         download(d["bucket"], d["key"])
+    elif d["operation"] == "delete":
+        delete(d["bucket"], d["key"])
 
 if __name__=="__main__":
     cmd()
